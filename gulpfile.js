05645c4c8d8830d3a8a6c6ccfa70e90d9fcb32e7
marysiaK <marysia.kolaczynska@gmail.com>
2017-06-10 10:18:17 +0200

    basic sections

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var gulpStylelint = require('gulp-stylelint');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('lint-css', function lintCssTask() {
+  return gulp
+    .src('./sass/**/*.scss')
+    .pipe(gulpStylelint({
+      reporters: [
+        {formatter: 'string', console: true}
+      ]
+    }));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass', 'lint-css']);
+});
---
+++ js/main.js
@@ -0,0 +1,9 @@
+(function() {
+
+	 //slick slider
+    $('.hero-slider').slick({
+      autoplay: true,
+      arrows: false,
+      dots: true,
+    });
+})();
